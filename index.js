64af85235cc2b11047e45b2fe6ce135de630ba31
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2022-12-05 20:52:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const textTime = document.getElementById("clock");
+const circleText = document.getElementById("circle-one");
+
+function makeCircles(){
+    for (let i = 0; i<1440; i + 90){
+        let dateObj = new Date()
+        let newDate = new Date(dateObj.getTime() + i*60000);
+        let hh = newDate.getHours();
+        let mm = newDate.getMinutes();
+        //let ss = newDate.getSeconds();
+        let session ="AM";
+        if(hh == 0){
+            hh = 12;
+        } else if(hh > 12){
+            hh = hh - 12;
+            session = "PM";
+         }
+      
+         hh = (hh < 10) ? "0" + hh : hh;
+         mm = (mm < 10) ? "0" + mm : mm;
+         //ss = (ss < 10) ? "0" + ss : ss;
+         let newTime =  `${hh}:${mm} ${session}`;
+         circleText.textContent += newTime;
+    
+    }
+}
+ makeCircles();
+
+ //function texto(){
+ //circleText.textContent += "time";}
+
+//texto();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session ="AM"
+    if(hh == 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+     let time =  `${hh}:${mm}:${ss} ${session}`
+     textTime.textContent = time
+
+    let t = setTimeout(function(){ currentTime() }, 1000);
+    
+    
+}
+
+currentTime()
+
+function circle() {
+    let oldDateObj = new Date(); 
+    //let newDateObj = new Date(oldDateObj.getTime() + 90*60000);
+    let hoursFirstCicle = newDateObj.getHours();
+    let minutesFirstCicle = newDateObj.getMinutes();
+    let secondsFirstCicle = newDateObj.getSeconds();
+    let timeFirstCicle = `${hoursFirstCicle}:${minutesFirstCicle}:${secondsFirstCicle}`; 
+
+    circleOne.textContent = timeFirstCicle;
+}
+circle()*/
+
