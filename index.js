c3f25cd4601b6dec6a7dce06f94149822a9ab132
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2022-12-06 16:18:11 +0000

    intervals of 90 minutes during the day
    The app to calculate intervals of 90 minutes since the user presses the button

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
-const textTime = document.getElementById("clock");
-const circleText = document.getElementById("circle-one");
+const awakeBtn = document.getElementById('awake-btn');
+const circles = document.getElementById("circles")
+const formatBtn = document.getElementById("format-btn")
+let numbers = [];
+let n = 60000;
+let clockOption = true;
 
-function makeCircles(){
-    for (let i = 0; i<1440; i + 90){
-        let dateObj = new Date()
-        let newDate = dateObj.getTime() + i*60000;
-        console.log(newDate)
-        /*let hh = newDate.getHours();
-        let mm = newDate.getMinutes();
-        
-        let session ="AM";
-        if(hh === 0){
-            hh = 12;
-        } else if(hh > 12){
-            hh = hh - 12;
-            session = "PM";
-        }
-      
-         hh = (hh < 10) ? "0" + hh : hh;
-         mm = (mm < 10) ? "0" + mm : mm;
-         let newTime =  `${hh}:${mm} ${session} - `;
-         circleText.textContent += newTime;*/
-    
-    }
+formatBtn.addEventListener("click", function(){
+  if(clockOption){
+    clockOption = false;
+    opt();
+  }else{
+    clockOption = true;
+    opt();
+  }
+})
+function opt(){
+  numbers = [];
+  let counter = 0;
+  while (counter < 1440) {
+    let nn = counter*n;
+    numbers.push(nn);
+    counter += 90;
+  }
+  if(clockOption){
+    render();
+  }else{
+    renderTwentyFour();
+  }
 }
- makeCircles();
+function render(){
+  let setTime = Date.parse(new Date);
+  let tex = '';
+  for(let i = 0; i<numbers.length; i++){
+    let cicle =  numbers[i] + setTime;
+    let cicleCompleted = new Date(cicle)
+    let hh = cicleCompleted.getHours();
+    let mm = cicleCompleted.getMinutes();
+    let session = "am"
+    if(hh>12){
+      hh-= 12
+      session="pm"
+    } if (hh==12){
+      session="pm"
+    }if (hh<10){
+      hh = "0"+hh
+    }
+    if(mm<10){
+      mm = "0"+mm
+    }
+    tex = tex + ` <li> ${hh} : ${mm}  ${session}</li> `;
+    }
+    circles.innerHTML = tex;
+}
+function renderTwentyFour(){
+  let setTime = Date.parse(new Date);
+  let tex = '';
+  for(let i = 0; i<numbers.length; i++){
+    let cicle =  numbers[i] + setTime;
+    let cicleCompleted = new Date(cicle)
+    let hh = cicleCompleted.getHours();
+    let mm = cicleCompleted.getMinutes();
+    
+    if (hh<10){
+      hh = "0"+hh
+    }
+    if(mm<10){
+      mm = "0"+mm
+    }
+    tex = tex + ` <li> ${hh} : ${mm} </li> `;
+    }
+    circles.innerHTML = tex;
+}
 
